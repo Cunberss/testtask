626813482f53c88988ee7536fa26f12c3944e98f
Cunberss <86554178+Cunberss@users.noreply.github.com>
2024-04-14 19:35:45 +0300

    Update mongo_client.py

--- src/db/mongo_client.py
+++ src/db/mongo_client.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
-from src.config import DATABASE_NAME, DATABASE_COLLECTION
+from src.config import DATABASE_NAME, DATABASE_COLLECTION, DATABASE_URL
 
 
-client = MongoClient()
+client = MongoClient(DATABASE_URL)
 db = client[DATABASE_NAME]
 collection = db[DATABASE_COLLECTION]
